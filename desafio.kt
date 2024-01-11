810d0ecba927bc5725f5272af29ad8061b7a6eea
Marcos <129610771+MarcosSMZ94@users.noreply.github.com>
2024-01-11 16:04:57 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -46,7 +46,7 @@ data class Formacao(val nome: String,  val nivel: Nivel, var conteudos: MutableL
         println("Conteúdos:")
         for (conteudo in conteudos) {
             println()
-            println("${conteudo.nome} --- Duracao de ${conteudo.duracao} minutos\n${conteudo.descricao}\n(Criado por ${conteudo.autor})")
+            println("${conteudo.nome} --- Duracao de ${conteudo.duracao} minutos\n${conteudo.descricao}\nCriado por ${conteudo.autor}")
         }
         println()
         println("Inscritos:")
